157f99decbb6f234794c4c41c4763becb45e5b33
Stefan <stefan@layereight.de>
2019-11-28 18:14:04 +0100

    work in progress

--- MPU6050-trigger.py
+++ MPU6050-trigger.py
@@ -2,48 +2,65 @@
 
 from mpu6050 import mpu6050
 from time import sleep
+import os
 import logging
+import logging.config
 import subprocess
+import json
 
-logging.basicConfig(level=logging.DEBUG)
+logging.config.fileConfig(os.path.dirname(__file__) + '/logging.ini')
 logging.info("Start")
 
 
 def next():
+    process = subprocess.Popen(["/usr/local/bin/volumio", "status"], stdout=subprocess.PIPE)
+    bla = json.load(process.stdout)
+
+    print(bla['status'])
+    if bla['status'] != "play":
+        return
     logging.info("next")
     subprocess.call(['volumio', 'next'], shell=False)
 
 
 def prev():
+    process = subprocess.Popen(["/usr/local/bin/volumio", "status"], stdout=subprocess.PIPE)
+    bla = json.load(process.stdout)
+
+    print(bla['status'])
+    if bla['status'] != "play":
+        return
+
     logging.info("prev")
+    subprocess.call(['volumio', 'previous'], shell=False)
     subprocess.call(['volumio', 'previous'], shell=False)
 
 
 sensor = mpu6050(0x68)
 
-# threshold_left = -0.5
-# threshold_right = 0.5
+threshold_left = -3.0
+threshold_right = 3.0
 
-# accel_data = sensor.get_accel_data()
+accel_data = sensor.get_accel_data()
 gyro_data = sensor.get_gyro_data()
 
 lastGyroX = gyro_data['x']
 lastGyroY = gyro_data['y']
 lastGyroZ = gyro_data['z']
-# lastY = accel_data['y']
+lastY = accel_data['y']
 # lastY2 = lastY
 # lastY3 = lastY2
 threshold = 40.0
 
 while True:
-    # accel_data = sensor.get_accel_data()
+    accel_data = sensor.get_accel_data()
     gyro_data = sensor.get_gyro_data()
     #temp = sensor.get_temp()
 
 
 
     # currentX = accel_data['x']
-    # currentY = accel_data['y']
+    currentY = accel_data['y']
     # currentZ = accel_data['z']
 
     gyroX = gyro_data['x'] + 12# * gyro_data['x']
@@ -60,7 +77,7 @@ while True:
     # print("ly3: " + str(lastY3))
     # print("ly2: " + str(lastY2))
     # print(" ly: " + str(lastY))
-    # print("  y: " + str(currentY))
+    print("  y: " + str(currentY))
     # print("dy3: " + str(deltaY3))
     # print("dy2: " + str(deltaY2))
     # print(" dy: " + str(deltaY))
@@ -83,16 +100,18 @@ while True:
     # elif 1.0 < currentY:
     #     event = "right"
 
-    if gyroZ > threshold and lastGyroZ < threshold and gyroY < 120 and gyroX < 120:
+    # if gyroZ > threshold and lastGyroZ < threshold and gyroY < 120 and gyroX < 120:
+    if currentY < threshold_left and lastY > threshold_left and gyroY < 120:# and gyroX < 120:
         event = "left"
         prev()
-    elif gyroZ < -threshold and lastGyroZ > -threshold and gyroY < 120 and gyroX < 120:
+    # elif gyroZ < -threshold and lastGyroZ > -threshold and gyroY < 120 and gyroX < 120:
+    elif currentY > threshold_right and lastY < threshold_right and gyroY < 120:# and gyroX < 120:
         event = "right"
         next()
     #
     # lastGyroX = gyroX
     # lastGyroY = gyroY
-    # lastGyroZ = gyroZ
+    lastGyroZ = gyroZ
 
 
     # print("x: " + str(currentX))
@@ -105,7 +124,7 @@ while True:
 
     # lastY3 = lastY2
     # lastY2 = lastY
-    # lastY = currentY
+    lastY = currentY
 
     # print("\033[F\033[F\033[F\033[F\033[F\033[F\033[F\033[F\033[F")
 
