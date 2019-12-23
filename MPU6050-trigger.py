d366d16bd956b503ce1ed441983ba14b66fe335a
Stefan <stefan@layereight.de>
2019-12-23 20:42:15 +0100

    migrate to python 3, improve gesture detection

--- MPU6050-trigger.py
+++ MPU6050-trigger.py
@@ -1,154 +1,105 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding: utf8 -*-
 
 from mpu6050 import mpu6050
-from time import sleep
+import time
 import os
 import logging
 import logging.config
 import subprocess
 import json
+from enum import Enum, unique
 
 logging.config.fileConfig(os.path.dirname(__file__) + '/logging.ini')
 logging.info("Start")
 
-DEVNULL = open(os.devnull, 'wb')
+
+@unique
+class AccelCalibration(Enum):
+    X = -0.375
+    Y = -0.25
+    Z = 9.3
+
+
+THRESHOLD_X_FORWARD = AccelCalibration.X.value - 3.5
+THRESHOLD_X_BACK = AccelCalibration.X.value + 3.5
+
+TOLERANCE_Y = 0.45
+
+MIN_Y = AccelCalibration.Y.value - TOLERANCE_Y
+MAX_Y = AccelCalibration.Y.value + TOLERANCE_Y
+
+
+MIN_Z = 0
+MAX_Z = AccelCalibration.Z.value - 1.0
 
 
 def volumio_status():
     process = subprocess.Popen(["/usr/local/bin/volumio", "status"], stdout=subprocess.PIPE)
-    return json.load(process.stdout)
+    return json.loads(str(process.stdout.read(), "utf-8"))
 
 
 def next():
+    logging.info("next")
     volumio = volumio_status()
-
-    # print(bla['status'])
     if volumio['status'] != "play":
         return
-    logging.info("next")
-    subprocess.call(['volumio', 'next'], shell=False, stdout=DEVNULL, stderr=DEVNULL)
+    subprocess.call(['volumio', 'next'], shell=False, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 
 def prev():
+    logging.info("prev")
     volumio = volumio_status()
-    # print(bla['status'])
     if volumio['status'] != "play":
         return
+    subprocess.call(['volumio', 'previous'], shell=False, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.call(['volumio', 'previous'], shell=False, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-    logging.info("prev")
-    subprocess.call(['volumio', 'previous'], shell=False, stdout=DEVNULL, stderr=DEVNULL)
-    subprocess.call(['volumio', 'previous'], shell=False, stdout=DEVNULL, stderr=DEVNULL)
+
+def in_range(value, min, max):
+    return min < value < max
+
+
+def log_raw_data(accel_data, gyro_data = None, temp = None):
+    logging.debug("Accelerometer data")
+    logging.debug("x: " + str(accel_data["x"]))
+    logging.debug("y: " + str(accel_data["y"]) + "[" + str(MIN_Y) + ", " + str(MAX_Y) + "]")
+    logging.debug("z: " + str(accel_data["z"]) + "[" + str(MIN_Z) + ", " + str(MAX_Z) + "]")
+    if gyro_data is not None:
+        logging.debug("Gyroscope data")
+        logging.debug("gyro x: " + str(gyro_data["x"]))
+        logging.debug("gyro y: " + str(gyro_data["y"]))
+        logging.debug("gyro z: " + str(gyro_data["z"]))
+    if temp is not None:
+        logging.debug("Temp: " + str(temp) + " C")
 
 
 sensor = mpu6050(0x68)
 
-threshold_left = -3.0
-threshold_right = 3.0
-
 accel_data = sensor.get_accel_data()
-gyro_data = sensor.get_gyro_data()
-
-lastGyroX = gyro_data['x']
-lastGyroY = gyro_data['y']
-lastGyroZ = gyro_data['z']
-lastY = accel_data['y']
 lastX = accel_data['x']
-# lastY2 = lastY
-# lastY3 = lastY2
-threshold = 40.0
+lastY = accel_data['y']
+lastZ = accel_data['z']
 
 while True:
     accel_data = sensor.get_accel_data()
-    gyro_data = sensor.get_gyro_data()
-    #temp = sensor.get_temp()
-
-
+    # gyro_data = sensor.get_gyro_data()
+    # temp = sensor.get_temp()
 
     currentX = accel_data['x']
-    # currentY = accel_data['y']
-    # currentZ = accel_data['z']
+    currentY = accel_data['y']
+    currentZ = accel_data['z']
 
-    gyroX = gyro_data['x'] + 12# * gyro_data['x']
-    gyroX = gyroX * gyroX
-    gyroY = gyro_data['y'] * gyro_data['y']
-    gyroZ = gyro_data['z'] + 1.7
+    log_raw_data(accel_data)
+    # log_raw_data(accel_data, gyro_data, temp)
 
-    # currentY = currentY * currentY * currentY + currentY * currentY
-
-    # deltaY = currentY - lastY
-    # deltaY2 = lastY - lastY2
-    # deltaY3 = lastY2 - lastY3
-
-    # print("ly3: " + str(lastY3))
-    # print("ly2: " + str(lastY2))
-    # print(" ly: " + str(lastY))
-    # print("  y: " + str(currentY))
-    # print("  x: " + str(currentX))
-    # print("dy3: " + str(deltaY3))
-    # print("dy2: " + str(deltaY2))
-    # print(" dy: " + str(deltaY))
-
-
-
-    # sum = currentX + currentY + currentZ
-
-
-
-    # event = ""
-
-    # if deltaY < threshold_left and deltaY2 < threshold_left and deltaY3 < threshold_left:
-    #     event = "left"
-    # elif deltaY > threshold_right and deltaY2 > threshold_right and deltaY3 > threshold_right:
-    #     event = "right"
-
-    # if -1.0 > currentY:
-    #     event = "left"
-    # elif 1.0 < currentY:
-    #     event = "right"
-
-    # if gyroZ > threshold and lastGyroZ < threshold and gyroY < 120 and gyroX < 120:
-    if currentX < threshold_left and lastX > threshold_left and gyroX < 120:# and gyroX < 120:
-        event = "left"
+    if in_range(THRESHOLD_X_FORWARD, currentX, lastX) and in_range(currentY, MIN_Y, MAX_Y) and in_range(currentZ, MIN_Z, MAX_Z):
         prev()
-    # elif gyroZ < -threshold and lastGyroZ > -threshold and gyroY < 120 and gyroX < 120:
-    elif currentX > threshold_right and lastX < threshold_right and gyroX < 120:# and gyroX < 120:
-        event = "right"
+    elif in_range(THRESHOLD_X_BACK, lastX, currentX) and in_range(currentY, MIN_Y, MAX_Y) and in_range(currentZ, MIN_Z, MAX_Z):
         next()
-    #
-    # lastGyroX = gyroX
-    # lastGyroY = gyroY
-    lastGyroZ = gyroZ
 
-
-    # print("x: " + str(currentX))
-    # print("y: " + str(currentY) + " " + event)
-    # print("")
-    # print("z: " + str(accel_data['z']))
-    # print("sum: " + str(sum))
-    # print("  e: " + event)
-
-
-    # lastY3 = lastY2
-    # lastY2 = lastY
-    # lastY = currentY
     lastX = currentX
+    lastY = currentY
+    lastZ = currentZ
 
-    # print("\033[F\033[F\033[F\033[F\033[F\033[F\033[F\033[F\033[F")
-
-    # print("Accelerometer data")
-    # print("x: " + str(accel_data['x']))
-    # print("y: " + str(currentY))
-    # print("z: " + str(accel_data['z']))
-    #
-    #
-    # print("Gyroscope data")
-    # print("gyro x: " + str(gyroX))
-    # print("gyro y: " + str(gyroY))
-    # print("gyro z: " + str(gyroZ) + " " + event)
-
-
-    # print("")
-
-    #    print("Temp: " + str(temp) + " C")
-    sleep(0.1)
+    time.sleep(0.1)
